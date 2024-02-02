26902eb94a4f89e617cc075064b49dc2401ae2cb
yaminidevi-mp <yaminidevimp@gmail.com>
2024-02-02 17:46:29 +0530

    test commit

---
+++ example/src/example/Factorial.java
@@ -0,0 +1,12 @@
+package example;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		
+	}
+
+}
